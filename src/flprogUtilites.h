418636b34646f6747ef6f7f05b7a0144ab6d7ed3
Totuin <sglushenco@yandex.ru>
2023-02-15 21:23:16 +0500

    Update flprogUtilites.h

--- src/flprogUtilites.h
+++ src/flprogUtilites.h
@@ -54,8 +54,10 @@
 namespace flprog
 {
     bool isTimer(uint32_t startTime, uint32_t period);
+    bool isTimerMicros(unsigned long startTime, unsigned long period);
     int serialCodeForParametrs(byte portDataBits, byte portStopBits, byte portParity);
     uint32_t speedFromCode(byte code);
     uint8_t codeFromSpeed(int32_t speed);
-    bool isTimerMicros(unsigned long startTime, unsigned long period);
+
 };
+
