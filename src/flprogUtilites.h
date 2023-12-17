53fd8f92950de319ad85cc87db19967225e569f4
Totuin <117037443+Totuin@users.noreply.github.com>
2023-12-17 14:31:58 +0500

    Update flprogUtilites.h

--- src/flprogUtilites.h
+++ src/flprogUtilites.h
@@ -166,7 +166,7 @@ struct FLProgCompactUartStruct
 #define FLPROG_ETHERNET_SERVER_NOT_CALLBACK_ERROR 60
 #define FLPROG_ETHERNET_SERVER_SOKET_ERROR 61
 
-Serial.print('dssdaas');
+
 
 namespace flprog
 {
