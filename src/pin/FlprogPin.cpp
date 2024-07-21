2f65d6840e8901a7cb1413ef8de3e0d1a8da1924
Totuin <117037443+Totuin@users.noreply.github.com>
2024-07-21 05:55:50 +0400

    21-07-24

--- src/pin/FlprogPin.cpp
+++ src/pin/FlprogPin.cpp
@@ -1,5 +1,38 @@
 #include "FlprogPin.h"
 
+// -----------------------------------------------FlprogAbstractDiscreteInputPin---------------------------------------
+
+bool FlprogAbstractDiscreteInputPin::checkInvert(bool value)
+{
+    if (_inverted)
+    {
+        return !value;
+    }
+    return value;
+}
+
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+bool FlprogAbstractDiscreteInputPin::init()
+{
+    if (_pullMode == FLPROG_PULL_UP_MODE)
+    {
+        pinMode(number, INPUT_PULLUP);
+    }
+    else
+    {
+        if (_pullMode == FLPROG_PULL_DOWN_MODE)
+        {
+            pinMode(number, INPUT_PULLDOWN);
+        }
+        else
+        {
+            pinMode(number, INPUT);
+        }
+    }
+    _isInit = true;
+}
+#endif
+
 // -----------------------------------------------FlprogDiscreteInputPin---------------------------------------------
 FlprogDiscreteInputPin::FlprogDiscreteInputPin(uint8_t number, char pullMode, bool inverted)
 {
@@ -10,13 +43,15 @@ FlprogDiscreteInputPin::FlprogDiscreteInputPin(uint8_t number, char pullMode, bo
 
 bool FlprogDiscreteInputPin::digitalRead()
 {
-    bool temp;
-    temp = RT_HW_Base.pinDigitalRead(_structure, _number, _pullMode);
-    if (_inverted)
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    if (!_isInit)
     {
-        return !temp;
+        init();
     }
-    return temp;
+    return checkInvert(digitalRead(_number));
+#else
+    return checkInvert(RT_HW_Base.pinDigitalRead(_structure, _number, _pullMode));
+#endif
 }
 
 // -----------------------------------------------FlprogBounceDiscreteInputPin---------------------------------------------
@@ -29,14 +64,40 @@ FlprogBounceDiscreteInputPin::FlprogBounceDiscreteInputPin(uint8_t number, char 
 
 bool FlprogBounceDiscreteInputPin::digitalRead()
 {
-    bool temp;
-    temp = RT_HW_Base.pinDigitalRead(_structure, _number, _pullMode);
-    temp = RT_HW_Base.chatterDIN(_bounce, temp, 50);
-    if (_inverted)
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+
+    if (!_isInit)
     {
-        return !temp;
+        init();
+        _result = digitalRead(_number);
+        _oldValue = _result;
+        return checkInvert(_result);
     }
-    return temp;
+    bool temp = digitalRead(_number);
+    if (!(_oldValue == temp))
+    {
+        _startTime = millis();
+        _oldValue = temp;
+        return checkInvert(_result);
+    }
+    if (_oldValue == temp)
+    {
+        if (_oldValue == _result)
+        {
+            return checkInvert(_result);
+        }
+    }
+    if (flprog::isTimer(_startTime, 40))
+    {
+        if (_oldValue == temp)
+        {
+            _result = temp;
+            return checkInvert(_result);
+        }
+    }
+#else
+    return checkInvert(RT_HW_Base.chatterDIN(_bounce, (RT_HW_Base.pinDigitalRead(_structure, _number, _pullMode)), 50));
+#endif
 }
 
 // -----------------------------------------------FlprogDiscreteOutputPin---------------------------------------------
@@ -48,13 +109,63 @@ FlprogDiscreteOutputPin::FlprogDiscreteOutputPin(uint8_t number)
 void FlprogDiscreteOutputPin::digitalWrite(bool value)
 {
     _cash = value;
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    if (!_isInit)
+    {
+        pinMode(number, OUTPUT);
+        _isInit = true;
+    }
+    digitalWrite(number, _cash);
+#else
     RT_HW_Base.pinDigitalWrite(_structure, _number, _cash, 'N');
+#endif
 }
 
 // -----------------------------------------------FlprogShimOutputPin---------------------------------------------
 FlprogShimOutputPin::FlprogShimOutputPin(uint8_t number)
 {
     _number = number;
+#ifndef FLPROG_COMPACT_LIBRARY_MODE
     _structure.freq = 1000;
     _structure.mode = 'N';
+#endif
 }
+
+void FlprogShimOutputPin::analogWrite(uint16_t value)
+{
+    _cash = value;
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    analogWrite(_number, _cash);
+#else
+    RT_HW_Base.pinPWM(_structure, _number, _cash);
+#endif
+}
+
+// -----------------------------------------------FlprogAnalogInputPin---------------------------------------------
+FlprogAnalogInputPin::FlprogAnalogInputPin(uint8_t number)
+{
+    _number = number;
+}
+
+uint16_t FlprogAnalogInputPin::analogRead()
+{
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    return analogRead(_number)
+#else
+    return RT_HW_Base.pinAnalogRead(_structure, _number);
+#endif
+}
+
+// -----------------------------------------------FlprogAnalogInputPin---------------------------------------------
+#ifndef FLPROG_COMPACT_LIBRARY_MODE
+FlprogDacOutputPin::FlprogDacOutputPin(uint8_t number)
+{
+    _number = number;
+}
+
+void FlprogDacOutputPin::analogWrite(uint16_t value)
+{
+    _cash = value;
+    RT_HW_Base.pinDAC(_structure, _number, _cash);
+}
+#endif
--- src/pin/FlprogPin.h
+++ src/pin/FlprogPin.h
@@ -7,31 +7,42 @@
 #define FLPROG_PULL_DOWN_MODE 'D'
 #define FLPROG_PULL_NOT_MODE 'F'
 
-class FlprogDiscreteInputPin
+class FlprogAbstractDiscreteInputPin
+{
+protected:
+    char _pullMode;
+    uint8_t _number;
+    bool _inverted;
+    bool checkInvert(bool value);
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    bool _isInit = false;
+    void init();
+#else
+    RT_HW_PIN_DIR_ID _structure;
+#endif
+};
+
+class FlprogDiscreteInputPin : public FlprogAbstractDiscreteInputPin
 {
 public:
     FlprogDiscreteInputPin(uint8_t number, char pullMode = FLPROG_PULL_NOT_MODE, bool inverted = false);
     bool digitalRead();
-
-private:
-    char _pullMode;
-    uint8_t _number;
-    bool _inverted;
-    RT_HW_PIN_DIR_ID _structure;
 };
 
-class FlprogBounceDiscreteInputPin
+class FlprogBounceDiscreteInputPin : public FlprogAbstractDiscreteInputPin
 {
 public:
     FlprogBounceDiscreteInputPin(uint8_t number, char pullMode = FLPROG_PULL_NOT_MODE, bool inverted = false);
-    virtual bool digitalRead();
+    bool digitalRead();
 
-private:
-    uint8_t _number;
-    char _pullMode;
-    bool _inverted;
-    RT_HW_PIN_DIR_ID _structure;
+protected:
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    bool _oldValue = false;
+    uint32_t _startTime;
+    bool _result;
+#else
     RT_HW_CHATTER_DIN _bounce;
+#endif
 };
 
 class FlprogDiscreteOutputPin
@@ -41,18 +52,55 @@ public:
     virtual bool digitalRead() { return _cash; };
     void digitalWrite(bool value);
 
-private:
+protected:
     bool _cash = 0;
     uint8_t _number;
+#ifdef FLPROG_COMPACT_LIBRARY_MODE
+    bool _isInit = false;
+#else
     RT_HW_PIN_DIR_ID _structure;
+#endif
 };
 
 class FlprogShimOutputPin
 {
 public:
     FlprogShimOutputPin(uint8_t number);
+    void analogWrite(uint16_t value);
+    uint16_t analogRead() { return _cash; };
 
-private:
+protected:
     uint8_t _number;
+    uint16_t _cash = 0;
+#ifndef FLPROG_COMPACT_LIBRARY_MODE
     RT_HW_PIN_PWM_ID _structure;
+#endif
 };
+
+class FlprogAnalogInputPin
+{
+public:
+    FlprogAnalogInputPin(uint8_t number);
+    uint16_t analogRead();
+
+protected:
+    uint8_t _number;
+#ifndef FLPROG_COMPACT_LIBRARY_MODE
+    RT_HW_PIN_ADC_ID _structure;
+#endif
+};
+
+#ifndef FLPROG_COMPACT_LIBRARY_MODE
+class FlprogDacOutputPin
+{
+public:
+    FlprogDacOutputPin(uint8_t number);
+    uint16_t analogRead() { return _cash; };
+    void analogWrite(uint16_t value);
+
+protected:
+    uint8_t _number;
+    uint16_t _cash = 0;
+    RT_HW_PIN_DAC_ID _structure;
+};
+#endif
